6ef45a577afd77362e7b9fd24e7bb263128aeec9
rvstudioapp <56516657+rvstudioapp@users.noreply.github.com>
2020-04-14 19:02:22 +0530

    first commit

---
+++ app/src/main/java/us/koller/cameraroll/data/models/RAWImage.java
@@ -0,0 +1,28 @@
+package us.koller.cameraroll.data.models;
+
+import android.content.Context;
+import android.os.Parcel;
+
+import us.koller.cameraroll.R;
+import us.koller.cameraroll.util.Util;
+
+public class RAWImage extends Photo {
+
+    RAWImage() {
+
+    }
+
+    RAWImage(Parcel parcel) {
+        super(parcel);
+    }
+
+    @Override
+    public int[] retrieveImageDimens(Context context) {
+        return Util.getImageDimensions(context, getUri(context));
+    }
+
+    @Override
+    public String getType(Context context) {
+        return context.getString(R.string.raw_image);
+    }
+}
---
+++ app/src/main/java/us/koller/cameraroll/themes/BlackTheme.java
@@ -0,0 +1,11 @@
+package us.koller.cameraroll.themes;
+
+import us.koller.cameraroll.R;
+
+public class BlackTheme extends DarkTheme {
+
+    @Override
+    public int getBackgroundColorRes() {
+        return R.color.black_bg;
+    }
+}

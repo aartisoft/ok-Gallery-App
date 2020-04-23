24bbf25b3a6d4ea61d078deac7bfc2f303e45263
rvstudioapp <56516657+rvstudioapp@users.noreply.github.com>
2020-04-23 12:50:36 +0530

    Live ads set

--- app/src/main/java/com/gallery/album/App.java
+++ app/src/main/java/com/gallery/album/App.java
@@ -1,6 +1,10 @@
 package com.gallery.album;
 
 import android.app.Application;
+
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.onesignal.OneSignal;
 
 public class App extends Application {
@@ -13,6 +17,13 @@ public class App extends Application {
                 .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
                 .unsubscribeWhenNotificationsAreDisabled(true)
                 .init();
+        MobileAds.initialize(this,getString(R.string.admob_app_id));
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+
+            }
+        });
     }
 
     @Override
